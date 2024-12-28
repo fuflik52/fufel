d9199647650eec8d5a4caddcd3f8c3984c01989e
fuflik52 <klox4462@gmail.com>
2024-12-28 21:31:47 +0300

    Add files via upload

--- telegram.js
+++ telegram.js
@@ -12,7 +12,7 @@ class TelegramAuth {
         // Добавляем скрипт Telegram Widget
         const script = document.createElement('script');
         script.src = 'https://telegram.org/js/telegram-widget.js?22';
-        script.setAttribute('data-telegram-login', 'YOUR_BOT_NAME'); // Замените на имя вашего бота
+        script.setAttribute('data-telegram-login', 'CookieClicket_Bot'); // Замените на имя вашего бота
         script.setAttribute('data-size', 'large');
         script.setAttribute('data-onauth', 'onTelegramAuth(user)');
         script.setAttribute('data-request-access', 'write');
